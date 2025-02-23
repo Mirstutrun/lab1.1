6184c2a570b3497bbf47377b765f0bca1e484797
Алексей <@DESKTOP-935M4IQ>
2025-02-23 23:25:05 +0800

    полная декомпозиция и исправленный код

--- lab1.1/Program.cs
+++ lab1.1/Program.cs
@@ -1,39 +1,65 @@
-﻿Console.Write("Введите количество копеек: ");
-var chislo = int.Parse(Console.ReadLine());
+﻿
+namespace FirstApp
+{
+    public class Logic
+    {
+
+        public static string CountMoney(int chislo)
+        {
+            var rubli = chislo / 100;
+            var kopeiki = chislo % 100;
+            string outRuble = "";
+            string outKopeiki = "";
+            string outMessage = "";
+            if (rubli == 0)
+            { }
+            else if ((rubli == 1) || ((rubli % 10 == 1) && (rubli / 10 != 1)))
+            {
+                outRuble = rubli + " рубль ";
+            }
+            else if (((rubli > 1) && (rubli <= 4)) || ((rubli % 10 > 1) && (rubli % 10 < 5)) && !((rubli >= 12) && (rubli <= 14)))
+            {
+                outRuble = rubli + " рубля ";
+            }
+            else
+            {
+                outRuble = rubli + " рублей ";
+            }
+            if (kopeiki == 0)
+            { }
+            if ((kopeiki == 1) || ((kopeiki % 10 == 1) && (kopeiki / 10 != 1)))
+            {
+                outKopeiki = kopeiki + " копейка ";
+            }
+            else if (((kopeiki > 1) && (kopeiki <= 4)) || ((kopeiki % 10 > 1) && (kopeiki % 10 < 5))&& !((kopeiki>=12)&&(kopeiki<=14)))
+            {
+                outKopeiki = kopeiki + " копейки ";
+            }
+            else
+            {
+                outKopeiki = kopeiki + " копеек ";
+            }
+            outMessage = outRuble + outKopeiki;
+            return outMessage;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         Console.Write("Введите количество копеек: ");
+         var chislo = int.Parse(Console.ReadLine());
+         var outMessage = Logic.CountMoney(chislo);
+            Console.WriteLine(outMessage);
+        }
+    }
+}
+
+          
 
 
-var rubli = chislo / 100;
-var kopeiki = chislo % 100;
-string outRuble = "";
-string outKopeiki = "";
-if (rubli == 0)
-{ }
-else if ((rubli == 1) || ((rubli % 10 == 1)&&(rubli / 10 != 1)))
-{
-   outRuble=" рубль ";
-}
-else if (((rubli > 1) && (rubli <= 4)) || ((rubli / 10 > 1) && (rubli / 10 < 5)))
-{
-    outRuble = " рубля ";
-}
-else
-{
-    outRuble = " рублей ";
-}
-if (kopeiki == 0)
-{ }
-if ((kopeiki == 1) || (kopeiki % 10 == 1))
-{
-    outKopeiki =" копейка ";
-}
-else if (((kopeiki > 1) && (kopeiki <= 4)) || ((kopeiki % 10 > 1) && (kopeiki % 10 < 5)))
-{
-    outKopeiki = " копейки ";
-}
-else
-{
-    outKopeiki = " копеек ";
-}
 
 
-Console.Write(rubli + outRuble + kopeiki + outKopeiki);
+
