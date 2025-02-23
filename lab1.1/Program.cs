31bc3e82930de29ef6a83f49c7286b6cfdb9337e
Алексей <@DESKTOP-935M4IQ>
2025-02-23 22:29:45 +0800

    Ввод и вывод рублей

--- lab1.1/Program.cs
+++ lab1.1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество копеек: ");
+var chislo = int.Parse(Console.ReadLine());
+var rubli = chislo / 100;
+var kopeiki = chislo % 100;
+
+if (rubli == 0)
+{ }
+else if ((rubli == 1) || ((rubli % 10 == 1)&&(rubli / 10 != 1)))
+{
+    Console.Write(rubli + " рубль ");
+}
+else if (((rubli > 1) && (rubli <= 4)) || ((rubli / 10 > 1) && (rubli / 10 < 5)))
+{
+    Console.Write(rubli + " рубля ");
+}
+else
+{
+    Console.Write(rubli + " рублей ");
+}
+
