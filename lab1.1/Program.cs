427c35fe9ae67e86610b689e13b9f6b0a10d84f0
Алексей <@DESKTOP-935M4IQ>
2025-02-23 22:47:43 +0800

    Декомпозиция

--- lab1.1/Program.cs
+++ lab1.1/Program.cs
@@ -1,34 +1,39 @@
 ﻿Console.Write("Введите количество копеек: ");
 var chislo = int.Parse(Console.ReadLine());
+
+
 var rubli = chislo / 100;
 var kopeiki = chislo % 100;
-
+string outRuble = "";
+string outKopeiki = "";
 if (rubli == 0)
 { }
 else if ((rubli == 1) || ((rubli % 10 == 1)&&(rubli / 10 != 1)))
 {
-    Console.Write(rubli + " рубль ");
+   outRuble=" рубль ";
 }
 else if (((rubli > 1) && (rubli <= 4)) || ((rubli / 10 > 1) && (rubli / 10 < 5)))
 {
-    Console.Write(rubli + " рубля ");
+    outRuble = " рубля ";
 }
 else
 {
-    Console.Write(rubli + " рублей ");
+    outRuble = " рублей ";
 }
-
 if (kopeiki == 0)
 { }
 if ((kopeiki == 1) || (kopeiki % 10 == 1))
 {
-    Console.Write(kopeiki + " копейка ");
+    outKopeiki =" копейка ";
 }
 else if (((kopeiki > 1) && (kopeiki <= 4)) || ((kopeiki % 10 > 1) && (kopeiki % 10 < 5)))
 {
-    Console.Write(kopeiki + " копейки ");
+    outKopeiki = " копейки ";
 }
 else
 {
-    Console.Write(kopeiki + " копеек ");
+    outKopeiki = " копеек ";
 }
+
+
+Console.Write(rubli + outRuble + kopeiki + outKopeiki);
